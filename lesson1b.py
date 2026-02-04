feae805d9e8a0a4b680aa154064cc34cdb4667ac
chanyamungai <chanyamungai01@gmail.com>
2026-02-04 13:22:17 +0300

    added lesson1b

---
+++ lesson1b.py
@@ -0,0 +1,16 @@
+# Python data types
+# A sting is a series of characters that are inside of quotes
+name = "Joseph Kainembuga"
+print(name)
+print(type(name))
+# Numeric
+# Int- indicates a whole number in python we dont use quotes
+age=30
+print("The age of this person is:",age)
+print(type(age))
+
+# Float- this is a number with a decimal
+coordinates=15.4524
+print("The coordinates are:",coordinates)
+print(type(coordinates))
+
